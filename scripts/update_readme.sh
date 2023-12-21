fa772d664a275d43fb56ef84071dc0378cde99bd
Gopika <kamalgopika54@gmail.com>
2023-12-21 09:33:18 +0530

    changed

--- scripts/update_readme.sh
+++ scripts/update_readme.sh
@@ -1,59 +1,33 @@
 #!/bin/bash
 
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US.UTF-8
 export LC_TIME=en_US.UTF-8
 export TZ=Asia/Kolkata
 
 kelvin_to_celsius() {
-    temperature_kelvin=$1
-    temperature_celsius=$(echo "$temperature_kelvin - 273.15" | bc)
-    printf "%.2f" "$temperature_celsius"
+    echo "scale=2; $1 - 273.15" | bc
 }
 
-get_weather_icon() {
-    case $1 in
-        "Clear")
-            echo "☀️"
-            ;;
-        "Clouds")
-            echo "☁️"
-            ;;
-        "Rain")
-            echo "🌧️"
-            ;;
-        "Snow")
-            echo "❄️"
-            ;;
-        *)
-            echo ""
-            ;;
-    esac
-}
-
-indian_time=$(TZ=Asia/Kolkata date +'%Y-%m-%d %H:%M:%S %Z')
-
+indian_time=$(date +'%Y-%m-%d %H:%M:%S %Z')
 
 city="Coimbatore"
-weather_info=$(curl -s "http://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${OPENWEATHERMAP_API_KEY}")
+weather_info=$(curl -s "http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPENWEATHERMAP_API_KEY}")
 
+temperature_kelvin=$(echo $weather_info | jq -r '.main.temp')
+temperature_celsius=$(kelvin_to_celsius $temperature_kelvin)
+condition=$(echo $weather_info | jq -r '.weather[0].description')
 
-next_24_hours_data=$(echo "$weather_info" | jq -r '.list[] | select(.dt_txt | strptime("%Y-%m-%d %H:%M:%S") | mktime >= now and strptime("%Y-%m-%d %H:%M:%S") | mktime <= (now + (24*60*60))) | {dt_txt, main: .main, weather: .weather[0]}')
-
-formatted_weather=""
-while read -r line; do
-    timestamp=$(echo $line | jq -r '.dt_txt')
-    temperature_kelvin=$(echo $line | jq -r '.main.temp')
-    temperature_celsius=$(kelvin_to_celsius "$temperature_kelvin")
-    condition=$(echo $line | jq -r '.weather[0].description')
-
-    weather_icon=$(get_weather_icon "$condition")
-
-    formatted_weather+="| $timestamp | $temperature_celsius °C | $weather_icon $condition |\n"
-done <<< "$next_24_hours_data"
-
-echo "# Weather Forecast for the Next 24 Hours" > README.md
+echo "# My Project" > README.md
 echo -e "\nThis content is dynamically generated in Indian Time (IST): $indian_time\n" >> README.md
-echo -e "| Time | Temperature | Condition |\n| --- | --- | --- |\n$formatted_weather" >> README.md
+echo -e "\nCurrent Weather in $city:\nTemperature: $temperature_celsius °C\nCondition: $condition" >> README.md
+
+git config --global user.email "action@github.com"
+git config --global user.name "GitHub Action"
 
 git add README.md
-git commit -m "Update README with weather forecast for the next 24 hours"
-git push
+git commit -m "Update README with dynamic content"
+git pull origin main
+git push origin main
+
